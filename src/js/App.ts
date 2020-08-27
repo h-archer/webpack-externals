ab3410f5a58160b647b0a8313f102c8783403445
H Archer <hamadarcher1@gmail.com>
2020-08-27 22:34:06 +0300

    Adding files

---
+++ src/js/App.ts
@@ -0,0 +1,22 @@
+﻿import { fabric } from "fabric";
+import $ from 'jquery';
+
+export default class App {
+  
+    canvas: fabric.Canvas;
+
+    constructor(private readonly selector: string, canvasHeight: number, canvasWidth: number) {
+      
+      // Error
+      // Uncaught TypeError: jquery__WEBPACK_IMPORTED_MODULE_1___default(...) is not a function
+      $(`#${selector}`).replaceWith(`<canvas id="${selector}" height=${canvasHeight} width=${canvasWidth}> </canvas>`);
+
+      // Error:
+      // Uncaught TypeError: Cannot read property 'Canvas' of undefined
+      this.canvas = new fabric.Canvas(`${selector}`, { selection: false });
+
+    }
+
+}
+
+export { App };
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/js/App.ts',
+  output: {
+    globalObject: 'this',
+    umdNamedDefine: true,
+    library: 'mylib',
+    libraryTarget: 'umd', // or var, amd, window, global... whatever will give me access from a script tag to mylib
+    path: path.resolve(__dirname, 'public'),
+    filename: 'js/app.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'), 
+    watchContentBase: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /(node_module|public)/,
+        use: [
+          {
+            loader: 'ts-loader', 
+            options: { 
+              // transpileOnly: true,
+            }
+          }
+        ]
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  devtool: 'inline-source-map',
+  externals: {
+    jquery: 'JQuery',
+    fabric: 'fabric',
+  },
+};
